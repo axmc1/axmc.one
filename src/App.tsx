7d1d0064f57ce673b8266ec2729e6a2c5ce3360b
Alex <alex@axmc.one>
2026-01-11 20:40:54 +0100

    icymiicw2 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const aroundNav: AroundNavHandler = (navigate, to, options) => {
 function App() {
   return (
     <Router aroundNav={aroundNav}>
-      <div className="flex flex-col h-screen overflow-hidden bg-[#0a0a0a] text-white">
+      <div className="flex flex-col h-full overflow-hidden bg-[#0a0a0a] text-white">
         <Navbar />
         <main className="flex-1 w-full flex justify-center xl:p-32 lg:p-16 p-8 overflow-y-auto">
           <div className="xl:w-2/5 lg:w-3/5 md:w-4/5 sm:w-full">
